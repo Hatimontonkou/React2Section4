a78513e0759d23c900a5fc698975393b9dd22883
Hatimontonkou <110019666+Hatimontonkou@users.noreply.github.com>
2022-10-28 17:06:30 +0900

    StyledJsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { CssModules } from "./compenents/CssModules";
 import { InlineStyle } from "./compenents/InlineStyle";
+import { StyledJsx } from "./compenents/StyledJsx";
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
